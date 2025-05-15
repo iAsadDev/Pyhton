9db92ce68547fcf05e6a82d23f8c684c8d4c57de
iAsadDev <malikasad0155@gmail.com>
2025-05-15 10:44:18 -0700

    First lecture

---
+++ First.py
@@ -0,0 +1,80 @@
+# Variables
+# A variable is a name given to a memory location in a program
+
+
+# var ='asad'
+# print(var)
+# A variable can be assigned a value using the assignment operator '='
+# The value of a variable can be changed by assigning a new value to it
+
+
+
+# name = 'new value'
+# quantity=34
+
+# print("My name is  :", name, "and my age is :", quantity)
+
+
+# a =3
+# b=5
+# c=b+54
+# if (c > 50) :{
+#     print("c is greater than 50")
+# }
+# print(a+b+c)    
+
+
+
+# num = 20
+# num/= 40
+
+# print(num)
+
+
+#logical operator
+
+# a = 4
+# b = 5
+
+# print(a > b)
+
+
+# var1 = True
+# var2 = False
+# print(var1 or var2)  # Logical Or
+
+#type conversion
+
+# a = 90
+
+# a = int(input("Enter First Value : "))
+# b = int(input("Enter Second Value : "))
+# c = a + b 
+# print("Sum of two numbers is : ", c)
+
+
+# name = str(input("name Likho: "))
+# age = int(input("Enter Age"))
+# marks = float(input("Marks : "))
+
+# print("Wellcome", name ,"Age" ,age ,"And my marks", marks)
+
+# num1 = int(input("Enter First Number : "))
+# num2 = int(input("Enter Second Number : "))
+# sum = (num1 + num2)
+# print(sum)
+
+# a = int(input("Enter length : "))
+# print("The length of the square is : ", a ** 2)
+
+# a = float(input("Enter one : "))
+# b = float(input("Enter sec : "))
+
+# c = (a + b) / 2
+# print("The average of two numbers is : ", c)
+
+
+# a = int(input("Enter First Number : "))
+# b = int(input("Enter Second Number : "))
+# c = (a >= b)
+# print("The value of c is : ", c)
