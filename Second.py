8594e6c5a525b2b60dd87cc0bce4b0f9da2d6fe1
iAsadDev <malikasad0155@gmail.com>
2025-05-16 01:15:35 -0700

    Day 2 done

--- Second.py
+++ Second.py
@@ -1,2 +1,108 @@
-str2 = "this is apna College's tutorial"
-print(len(str2))
+#String in Python
+
+# str2 = "this is apna College's tutorial"
+# # print(len(str2)
+# str1 = "this is apna College's tutorial"
+# str1 = "T" + str1[1:]
+# print(str1)
+
+#Slicing
+# data = str("Malik Asad Qayyum")
+# data = "ASP" + data[0:5]
+# print(data)
+
+# str1 = str("Malik Asad Qayyum")
+# print(str1.endswith("Qayyum"))
+
+
+# str1 = str("malik asad qayyum")
+# str1 =str1.capitalize()
+# print(str1)
+
+#Replace Function
+
+# str1 = str("malik asad qayyum")
+# str1 = str1.find("a")
+# # str1 = str1.replace("a", "op")
+# print(str1)
+
+
+# str1 = str("malik asad qayyum")
+# str1 = str1.count("a")
+# print(str1)
+
+# name = str(input("Enter your name: "))
+# print(len(name))
+
+# name = "$malik asad$$$$ qayyum$"
+# print(name.count("$"))
+
+
+#Conditional Statements
+# age = int(input("Enter your age: "))
+# if(age >=  18):
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+# light = str(input("Enter the light color: "))
+# if(light == "red"):
+#     print("Stop")
+# elif(light == "green"):
+#     print("Go")
+# elif(light == "yellow"):
+#     print("Wait")
+# else:
+#     print("Invalid Color")
+# # print("Invalid Color")
+
+# marks = int(input("Enter your marks: "))
+
+# if(marks >=90 and marks <=100):
+#     print("Grade A")
+# elif(marks >= 80):
+#     print("Grade B")
+# elif(marks >= 70):
+#     print("Grade C")
+# elif(marks >= 60):
+#     print("Grade D")
+# elif(marks < 50):
+#     print("Grade F")
+# else:
+#     print("Invalid Marks")
+
+
+# age = int(input("Enter age "))
+# if(age >= 18):
+#     if(age >=80):
+#         print("cannot Drive")
+#     else:
+#         print("can Drive")
+# else: 
+#     print("not eligible")
+
+
+
+# num = int(input("Enter Number "))
+
+# if(num % 2 == 0):
+#     print("Even")
+# else:
+#     print("Odd")
+
+# num1 = int(input("Enter 1st Number"))
+# num2 = int(input("Enter 2nd Number"))
+# num3 = int(input("Enter 3rd Number"))
+
+# if( num1 > num2 and num1 > num3):
+#     print("Num 1 is Greater")
+# elif(num2 > 1 and num2 > 3):
+#     print("Num 2 is greater")
+# else:
+#     print("num 3 is  winner")
+
+# x = int(input("Enter Number"))
+
+# if(x % 7 == 0):
+#     print("Multiple of 7")
+# else:
+#     print("not multiple")
