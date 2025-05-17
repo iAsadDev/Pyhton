c04ea9e0f244824542d706e9d24b5e0839b7761f
iAsadDev <malikasad0155@gmail.com>
2025-05-16 21:02:27 -0700

    Third Lecture

---
+++ Third.py
@@ -0,0 +1,19 @@
+#List and Tuples 
+
+# marks = [10, 20, 30, 40, 50]
+
+# print(marks)
+# print(len(marks)) 
+
+#List in python
+#A built in data type that is used to store
+#multiple values in a single variable
+#It can store elements of different data types(interger, float, string ,etc)
+
+Student = ["Asad" , 22 ,"Karor"]
+print(Student)
+Student[1] = "Ali"
+print(Student)
+
+#string are immuteable means don't change 
+#list is mutable  
