b27931a21fbd1c3cfae30ebb1740cfed9ec63128
iAsadDev <malikasad0155@gmail.com>
2025-05-16 22:45:29 -0700

    Third lecture List and tuples

--- Third.py
+++ Third.py
@@ -10,10 +10,55 @@
 #multiple values in a single variable
 #It can store elements of different data types(interger, float, string ,etc)
 
-Student = ["Asad" , 22 ,"Karor"]
-print(Student)
-Student[1] = "Ali"
-print(Student)
+# Student = ["Asad" , 22 ,"Karor"]
+# Student[1] = "Ali"
+# #string are immuteable means don't change 
+# #list is mutable  
+# Student.append("Karachi")
+# Student.reverse()
+# Student.sort(reverse=True)
+# Student.remove("Karachi")
+# Student[1] =23
+# print(Student)
+# Tuples  /////////////////
+#A tuple is a collection which is ordered and unchangeable.
+#Tuples allow duplicate members
 
-#string are immuteable means don't change 
-#list is mutable  
+
+# tub = (1,3,4,5,6,3)
+# print(tub)
+# print(tub[0])
+
+# emp = ("Hello" ,"World", 1, 2, 1,1,4, 5)
+# print(emp.count(1))
+# print(emp)
+
+#Pracrtice Question
+# movies = []
+# m1= str(input("Enter movie name 1: "))
+# m2= str(input("Enter movie name 2: "))
+# m3= str(input("Enter movie name 3: "))
+
+# movies.append(m1)
+# movies.append(m2)
+# movies.append(m3)
+
+
+# print(movies)
+
+
+# Question no 2
+
+# list=["m", "o", "m"]
+# copy = list.copy()
+# copy.reverse()
+# if(copy == list):
+#     print("This is Palindrome")
+# else:
+#     print("This is not Palindrome")
+
+# Question no 3
+students = ["c","d" ,"a", "b","f","a","d"]
+print(students.count("a"))
+students.sort()
+print(students)
