8b67c41e36da38b11bf9b7885f135c0ca5207bab
iAsadDev <malikasad0155@gmail.com>
2025-05-20 10:01:05 -0700

    Lecture 4

---
+++ Fourth.py
@@ -0,0 +1,10 @@
+#Dictionary in python
+# Dictionary are used to store data values in key:value pairs.
+# They are unordered, mutable(changeable) and do not allow duplicates keys.
+dict ={
+    "name"  : "Ali",
+    "age"   : 22,
+    "marks" : [10, 20, 30, 40, 50],
+}
+
+print(dict)
--- Third.py
+++ Third.py
@@ -1,61 +1,61 @@
 #List and Tuples 
 
-# marks = [10, 20, 30, 40, 50]
+marks = [10, 20, 30, 40, 50]
 
-# print(marks)
-# print(len(marks)) 
+print(marks)
+print(len(marks)) 
 
 #List in python
 #A built in data type that is used to store
 #multiple values in a single variable
 #It can store elements of different data types(interger, float, string ,etc)
 
-# Student = ["Asad" , 22 ,"Karor"]
-# Student[1] = "Ali"
+Student = ["Asad" , 22 ,"Karor"]
+Student[1] = "Ali"
 # #string are immuteable means don't change 
 # #list is mutable  
-# Student.append("Karachi")
-# Student.reverse()
-# Student.sort(reverse=True)
-# Student.remove("Karachi")
-# Student[1] =23
-# print(Student)
+Student.append("Karachi")
+Student.reverse()
+Student.sort(reverse=True)
+Student.remove("Karachi")
+Student[1] =23
+print(Student)
 # Tuples  /////////////////
 #A tuple is a collection which is ordered and unchangeable.
 #Tuples allow duplicate members
 
 
-# tub = (1,3,4,5,6,3)
-# print(tub)
-# print(tub[0])
+tub = (1,3,4,5,6,3)
+print(tub)
+print(tub[0])
 
-# emp = ("Hello" ,"World", 1, 2, 1,1,4, 5)
-# print(emp.count(1))
-# print(emp)
+emp = ("Hello" ,"World", 1, 2, 1,1,4, 5)
+print(emp.count(1))
+print(emp)
 
 #Pracrtice Question
-# movies = []
-# m1= str(input("Enter movie name 1: "))
-# m2= str(input("Enter movie name 2: "))
-# m3= str(input("Enter movie name 3: "))
+movies = []
+m1= str(input("Enter movie name 1: "))
+m2= str(input("Enter movie name 2: "))
+m3= str(input("Enter movie name 3: "))
 
-# movies.append(m1)
-# movies.append(m2)
-# movies.append(m3)
+movies.append(m1)
+movies.append(m2)
+movies.append(m3)
 
 
-# print(movies)
+print(movies)
 
 
 # Question no 2
 
-# list=["m", "o", "m"]
-# copy = list.copy()
-# copy.reverse()
-# if(copy == list):
-#     print("This is Palindrome")
-# else:
-#     print("This is not Palindrome")
+list=["m", "o" , "m"]
+copy = list.copy()
+copy.reverse()
+if(copy == list):
+    print("This is Palindrome")
+else:
+    print("This is not Palindrome")
 
 # Question no 3
 students = ["c","d" ,"a", "b","f","a","d"]
