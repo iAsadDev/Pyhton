c9ec7a4b54871718dbb4fbfbb8f73c8faa174ef7
iAsadDev <malikasad0155@gmail.com>
2025-07-07 23:21:52 +0500

    While loop complete

--- Loops.py
+++ Loops.py
@@ -1,7 +1,37 @@
 #Loops :
 #Loops are used to repeat instruction;
-count = 1
-while count <= 12:
-    print("Hello")
-    count += 1
-print(count)
+# i = 5
+# while i >= 1:
+#     i -= 1
+#     print(i)
+#1
+# i = 0
+# while i <=100 :
+#     print(i)
+#     i += 1
+# #2
+# i = 100
+# while i >= 0 :
+#     print(i)
+#     i -= 1
+#3
+# i= 1
+# n =int(input("Enter Number "))
+# while i <= 10 :
+#     print(i *n)
+#     i += 1 
+
+#4 Traverse 
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+# idx = 0
+# while idx < len(nums)  :
+#     print(nums[idx])
+#     idx +=1 
+#5 
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81,100)
+x=38
+i =0
+while i < len(nums) :
+    if(nums[i] == x):
+        print(i)
+    i +=1
